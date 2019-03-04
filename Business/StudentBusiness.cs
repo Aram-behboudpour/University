213ee6d28541531c39d20ea558899821abf6ad97
Mohsen <en.mohsenjafari@gmail.com>
2019-03-05 00:02:19 +0330

    edit student model and add student business! :D

---
+++ Business/StudentBusiness.cs
@@ -0,0 +1,39 @@
+﻿using Contract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business
+{
+    public class StudentBusiness
+    {
+        public string GetFullName(StudentModel model)
+        {
+            return $"{model.FirstName} {model.LastName}";
+        }
+
+        public string GetWelcome(StudentModel model)
+        {
+            return $"Welcome to university {model.FirstName}";
+        }
+        public string ConvertDateOfBirth(StudentModel model)
+        {
+            System.Globalization.PersianCalendar p = new System.Globalization.PersianCalendar();
+            var year = p.GetYear(model.DateOfBirth);
+            var month = p.GetMonth(model.DateOfBirth);
+            var day = p.GetDayOfMonth(model.DateOfBirth);
+            return $"{year}/{month.ToString().PadLeft('0')}/{day.ToString().PadLeft('0')}";
+        }
+        public int GetSalary(StudentModel model)
+        {
+            if (model.Age >= 20)
+            {
+                return 100;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
--- Contract/StudentModel.cs
+++ Contract/StudentModel.cs
@@ -9,8 +9,8 @@ namespace Contract
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Age { get; set; }
-        public string DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string FatherName { get; set; }
         public string PlaceOfBirth { get; set; }
     }
