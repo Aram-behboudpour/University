9e885c966264f093b35f78de43cfd22e0b23bb96
Solmaz Lotfi <solmaz.lotfi@gmail.com>
2019-03-04 17:08:28 +0330

    add AdmissionModel

---
+++ Contract/AdmissionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contract
+{
+    public class AdmissionModel
+    {
+       
+        public int Id { get; set; }
+        public string Course { get; set; }
+        public DateTime DateOfClose { get; set; }
+        public DateTime DateOfReopen { get; set; }
+        public int CapacityOfStudents { get; set; }
+        public int NumberOfSubmittedStudents { get; set; }
+
+    }
+}
