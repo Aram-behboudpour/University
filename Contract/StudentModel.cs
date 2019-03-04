e41952e6e0ce469820bcce59d6f5d733aaec4739
Mohsen <en.mohsenjafari@gmail.com>
2019-03-04 19:34:10 +0330

    Add student model :D

---
+++ Contract/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contract
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string DateOfBirth { get; set; }
+        public string FatherName { get; set; }
+        public string PlaceOfBirth { get; set; }
+    }
+}
