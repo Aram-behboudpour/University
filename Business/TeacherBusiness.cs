398eeb8e4338ab8c4e4e77b891d6e7f80f4d8528
samira <samira.kalaantari@gmail.com>
2019-03-05 01:52:43 -0800

    BusinessController

--- Business/TeacherBusiness.cs
+++ Business/TeacherBusiness.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,7 +8,30 @@ namespace Business
    public class TeacherBusiness
     {
         
+        public string GetFullName(TeacherModel Model)
 
+        {
+            string FullName;
+            FullName =  Model.FirstName + Model.LastName;
+            return $"Teacher FullName is : {FullName} ";
+
+           
+
+        }
+
+        public string ShowMessage ()
+        {
+            return "Welcome to university dear Arash";
+        }
+
+        public int GetSalary (TeacherModel Model)
+        {
+            if (Model.Age > 10)
+                return 1000;
+            else
+                return 500;
+
+        }
 
     }
 }
