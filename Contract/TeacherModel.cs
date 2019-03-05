d1adea929afa2591ecce5cbe12616e1fcc0fce0a
samira <samira.kalaantari@gmail.com>
2019-03-04 22:00:46 -0800

    Add TeacherModel

---
+++ Contract/TeacherModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contract
+{
+   public class TeacherModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FatherName { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public DateTime WorkingStartDate { get; set; }
+
+    }
+}
