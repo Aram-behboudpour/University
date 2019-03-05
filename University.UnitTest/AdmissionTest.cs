aeb1eed127374b686631294ca483e38070dcb919
Solmaz Lotfi <solmaz.lotfi@gmail.com>
2019-03-05 13:25:16 +0330

    Edit admissionModel(add duration) Edit admissionLogic(add all methods) Add admissionTest

---
+++ University.UnitTest/AdmissionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Contract;
+using Business;
+
+
+namespace University.UnitTest
+{
+    public class AdmissionTest
+    {
+
+        public void Test_Capacity_Exceed(string courseName)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+
+    }
+}
