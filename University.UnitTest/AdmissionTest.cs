06a3228ff438d0079bb2ed1f55aa95c149680415
Solmaz Lotfi <solmaz.lotfi@gmail.com>
2019-03-11 09:34:47 +0330

    admission test

--- University.UnitTest/AdmissionTest.cs
+++ University.UnitTest/AdmissionTest.cs
@@ -10,16 +10,89 @@ namespace University.UnitTest
 {
     public class AdmissionTest
     {
-
-        public void Test_Capacity_Exceed(string courseName)
+        private static DateTime _date;
+        public AdmissionTest()
         {
-            //Arrange
-
-            //Act
-
-            //Assert
+            _date = DateTime.Now;
         }
 
+        [Theory]
+        [InlineData("Math",20,15)]
+        public void Test_Capacity_Exceed(string courseName,int submited,int capacity)
+        {
+            //Arrange
+            var fakeModel = new AdmissionModel()
+            {
+                Course = courseName,
+                NumberOfSubmittedStudents=submited,
+                CapacityOfStudents=capacity
+            };
+            var fakeAdmissionLogic = new Business.AdmissionLogic();
+
+            //Act
+            var actualResult = fakeAdmissionLogic.CapacityExceed(fakeModel);
+
+            //Assert
+            Assert.Equal($"The course {fakeModel.Course} has reached the maximum size of capacity", actualResult);
+        }
+
+        [Theory]
+        [InlineData("Math",40,30)]
+        public void Test_Course_Specification(string courseName,int duration,int capacity)
+        {
+            //Arrange
+            var fakeModel = new AdmissionModel()
+            {
+                Course = courseName,
+                Duration = duration,
+                CapacityOfStudents=capacity
+            };
+            var fakeAdmissionLogic = new Business.AdmissionLogic();
+
+            //Act
+            var actualResult = fakeAdmissionLogic.CourseSpecification(fakeModel);
+
+            //Assert
+            Assert.Equal($"The Course '{fakeModel.Course}' will carry out in {fakeModel.Duration} hours with the number of {fakeModel.CapacityOfStudents} Students", actualResult);
+        }
+         
+       [Fact]
+        public void Test_Get_Persian_CloseDate()
+        {
+            //Arrange
+            var fakeModel = new AdmissionModel()
+            {
+                DateOfClose=_date
+            };
+            var fakeAdmissionLogic = new Business.AdmissionLogic();
+
+            //Act
+            var actualResult = fakeAdmissionLogic.GetPersianCloseDate(fakeModel);
+
+            //Assert
+            System.Globalization.PersianCalendar pdate = new System.Globalization.PersianCalendar();
+            var closeYear = pdate.GetYear(fakeModel.DateOfClose);
+            var closeMonth = pdate.GetMonth(fakeModel.DateOfClose);
+            var closeDay = pdate.GetDayOfMonth(fakeModel.DateOfClose);
+            Assert.Equal($"{closeYear}/{closeMonth.ToString().PadLeft('0')}/{closeDay.ToString().PadLeft('0')}", actualResult);
+        }
+
+       [Fact]
+        public void Test_Get_Georgian_CloseDate()
+        {
+            //Arrange
+            var fakeModel = new AdmissionModel()
+            {
+                DateOfClose = _date
+            };
+            var fakeAdmissionLogic = new Business.AdmissionLogic();
+
+            //Act
+            var actualResult = fakeAdmissionLogic.GetGeorgianCloseDate(fakeModel);
+
+            //Assert
+            Assert.Equal(fakeModel.DateOfClose, actualResult);
+        }
 
     }
 }
